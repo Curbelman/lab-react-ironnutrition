af57a6610e2bce75257e73305031c9def2449f23
Curbelman <111905398+Curbelman@users.noreply.github.com>
2023-01-12 21:01:03 +0100

    fixes antd import and completes iteration 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import foods from './foods.json';
+import foods from "./foods.json";
+import { useState } from 'react';
 
-function App() {
+
+function App() { 
+  const [foodArray, setFoodArray] = useState(foods);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+    {foodArray.map((eachFood, i) => {
+      return (
+        <div>
+        <p> {eachFood.name} </p>
+        <img src={eachFood.image} alt={eachFood.name} width="200px" />
+      </div>
+      );
+    })}
+    </div>
   );
 }
 
